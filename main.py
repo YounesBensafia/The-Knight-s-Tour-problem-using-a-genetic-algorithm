e07e765e8613adc4b5e95313032c49cf263ff770
Younes BENSAFIA <114447089+YounesBensafia@users.noreply.github.com>
2024-11-23 14:43:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+from  Population import Population
 from animation import animate
+# def main():
+#     population_size = 50
+#     population = Population(population_size)
+#     while True:
+#         population.check_population()
+#         bestSolution, maxFit= population.evaluate( )
+#         print("Best solution: ", maxFit)
+#         if maxFit == 64:
+#             print('done')
+#             break
+#         elif maxFit > 40:
+#             print('done')
+#             break
+#         population.create_new_generation ()
+ 
 
-def main():
-    example_path = [(0, 0), (2, 1), (4, 2), (6, 3), (7, 5), (5, 6), (3, 7), (1, 6)]
-    animate(example_path)
+def run_genetic_algorithm():
+    population = Population(population_size=50)
+    max_generations = 1000
+    while population.generation < max_generations:
+        population.check_population()
+        best_knight, fitness = population.evaluate()
+        print("Best solution: ", fitness)
+        if fitness == 64:
+            print(f"Solution found in generation {population.generation}!")
+            return(best_knight.path)
+        population.create_new_generation()
+
+movs = run_genetic_algorithm()
+animate(movs)
+
+# main()
